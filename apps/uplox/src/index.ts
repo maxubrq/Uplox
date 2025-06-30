9e681a1275f18bef35310c8961051c82eb557cc8
maxubrq <hungtp.play@gmail.com>
2025-07-01 03:03:46 +0700

    feat: remove presign

--- apps/uplox/src/index.ts
+++ apps/uplox/src/index.ts
@@ -1,26 +1,14 @@
 import { serve } from '@hono/node-server';
 import { getLogger, UploxLogger } from '@shared/logger';
 import { Hono } from 'hono';
-import { PresignRoutes } from '@presign/presentation';
-import { PresignService } from '@presign/application';
 import { AppEnv } from '@application/ports';
 import { UploxAppConfig, UploxAppConfigLoader } from '@application/app-config';
-import { MinioStorage } from '@presign/presentation/minio-storage';
-import { ClamavScanner } from '@presign/presentation/clamav-scanner';
 
-async function presignFeature(app: Hono<AppEnv>, logger: UploxLogger, config: UploxAppConfig) {
-    const minioStorage = await MinioStorage.getInstance(config, logger);
-    const clamavScanner = new ClamavScanner(logger);
-    const presignService = new PresignService(logger, config, minioStorage, clamavScanner);
-    const presignRoutes = new PresignRoutes(presignService, logger);
-    presignRoutes.attachRoutes(app);
-}
 
 async function bootstrap() {
     const app = new Hono<AppEnv>();
     const logger = getLogger('Uplox');
     const config = new UploxAppConfigLoader().loadFromEnv();
-    await presignFeature(app, logger, config);
     serve(
         {
             fetch: app.fetch,
