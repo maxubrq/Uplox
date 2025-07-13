3d655afffa62c62a53fb00336ea9e486ba963b56
maxubrq <hungtp.play@gmail.com>
2025-07-13 12:03:59 +0700

    feat: update

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,13 +10,17 @@ export default defineConfig({
             reportsDirectory: './coverage',
             include: ['apps/**/*.ts'],
         },
-        include: ['apps/**/*.spec.ts'],
+        include: ['apps/uplox/src/**/*.spec.ts'],
+        exclude: ['apps/scanner/src/**/*.spec.ts'],
     },
     resolve: {
         alias: {
             '@domain': path.resolve(__dirname, './apps/uplox/src/domain'),
             '@shared': path.resolve(__dirname, './apps/uplox/src/shared'),
             '@presign': path.resolve(__dirname, './apps/uplox/src/presign'),
+            '@infrastructure': path.resolve(__dirname, './apps/uplox/src/infrastructure'),
+            '@features': path.resolve(__dirname, './apps/uplox/src/features'),
+            '@application': path.resolve(__dirname, './apps/uplox/src/application'),
             '@': path.resolve(__dirname, './apps/uplox/src'),
         },
     },
